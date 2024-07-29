d1728d6db025fd797b7a01c586debd1a24a4475a
Danil Martyushev <martyusheff.danil@yandex.ru>
2024-07-29 21:20:28 +0300

    added constants Uri with URI prod

---
+++ src/main/java/site/nomoreparties/stellarburgers/constants/Uri.java
@@ -0,0 +1,5 @@
+package site.nomoreparties.stellarburgers.constants;
+
+public class Uri {
+    public static final String STELLARBURGERS_NOMOREPARTIES_PROD = "https://stellarburgers.nomoreparties.site/";
+}
